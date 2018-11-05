4eb788f8b4a355739b0a0c19cfbd4beacbfda338
Andrew Wilkinson <andywilkinson1993@gmail.com>
2018-11-05 21:59:27 +0000

    Post URL added (not currently working)

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -4,29 +4,69 @@ module.exports = {
   name: 'users',
   description: 'Returns the server members',
   execute(message, args, dogeService) {
-    let username = '';
-    if (args.length > 0) {
-      username = '/' + args[0];
-    } else if (args.length < 2) {
-      message.channel.send('Too many users requested, please only request one username.');
+    if (args.length < 0 && args.length > 2) {
+      return;
     }
-    dogeService
-      .doGet(apiPoints.users.get + username)
-      .then(result => {
-        let response = '';
 
-        if (result.data.users) {
-          let userArr = Object.entries(result.data.users);
-          userArr.forEach(u => {
-            response += u[1].username + ': ' + u[1].score + '\n\n';
+    let username = '';
+    let response = '```{0}```';
+
+    switch (args[0].toLowerCase()) {
+      case 'list':
+        dogeService
+          .doGet(`${apiPoints.users}`)
+          .then(result => {
+            let strBuild = '';
+
+            if (result.data.users) {
+              let userArr = Object.entries(result.data.users);
+              userArr.forEach(u => {
+                strBuild += `> User: ${u[1].username} - Score: ${u[1].score}\n\n`;
+              });
+              message.channel.send(response.replace('{0}', strBuild));
+            }
+          })
+          .catch(err => {
+            console.log('HELLO');
+            message.channel.send(err.toString());
           });
-        } else {
-          response += result.data.user.username + ': ' + result.data.user.score;
-        }
-        message.channel.send(response);
-      })
-      .catch(err => {
-        message.channel.send(err.toString());
-      });
+
+        break;
+
+      case 'profile':
+        username = args[1];
+
+        dogeService
+          .doGet(`${apiPoints.users}`, username)
+          .then(result => {
+            if (result.data.user) {
+              let strBuild = $`> User: ${result.data.user.username} - Score: ${result.data.user.score}`;
+              message.channel.send(response.replace('{0}', strBuild));
+            }
+          })
+          .catch(err => {
+            message.channel.send(err.toString());
+          });
+        break;
+      case 'create':
+        username = args[1];
+        let data = JSON.stringify({
+          username: username
+        });
+
+        dogeService
+          .doPost(`${apiPoints.users}`, data)
+          .then(result => {
+            message.channel.send($`User: <${username}> successfully created.`);
+          })
+          .catch(err => {
+            message.channel.send(err.toString());
+          });
+        break;
+
+      default:
+        message.channel.send(```!users list || !users profile <username> || !users create <username>```);
+        break;
+    }
   }
 };
--- src/service/service.js
+++ src/service/service.js
@@ -1,14 +1,26 @@
 import axios from 'axios';
 import { apiPoints } from './endpoints';
 
-const baseUrl = apiPoints.baseUrl
+const baseUrl = apiPoints.baseUrl;
 
 export default class DogeService {
-  async doGet(url, params) {
+  async doGet(url, params = '') {
     return await axios
       .get(`${baseUrl}${url}`, {
         params: {
           params
+        }
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+
+  async doPost(url, data) {
+    return await axios
+      .post(`${baseUrl}${url}`, data, {
+        headers: {
+          'Content-Type': 'application/json'
         }
       })
       .catch(function(error) {
